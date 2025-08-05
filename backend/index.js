59d9bcafc296fe8ed2eb65ebcc1e62171c2b24e6
giridharsmartbrew <giridhar.chennuru@smartbrew.in>
2025-08-05 12:08:41 +0530

    cors

--- backend/index.js
+++ backend/index.js
@@ -23,7 +23,16 @@ const app = express();
 passport.use('jwt', jwtStrategy);
 app.use(helmet());
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174'],
+  origin: config.env === 'production' 
+    ? [
+        'http://localhost:5173', 
+        'http://localhost:5174', 
+        'http://localhost:3000', 
+        'http://127.0.0.1:5173', 
+        'http://127.0.0.1:5174',
+        'https://smartspidy.smartbrew.in'
+      ]
+    : ['http://localhost:5173', 'http://localhost:5174', 'http://localhost:3000', 'http://127.0.0.1:5173', 'http://127.0.0.1:5174'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization', 'X-Requested-With'],
